476edc859ae39f240018d969495673f532c52a39
Ben <belinghy@gmail.com>
2017-12-02 11:53:43 -0800

    simple parser

--- handler.py
+++ handler.py
@@ -1,6 +1,33 @@
 # import services here
+import re
+from services import weather
 
 def parser(sms):
-	service = ''
-	params = []
-	return service, params
+    """
+    Weather Service Inputs:
+        weather <location> [day]
+        ex. weather vancouver now
+            weather vancouver today
+            weather vancouver tomorrow
+    """
+
+    service = ''
+    params = []
+
+    args = re.split('\s', sms.lower())
+    if len(args) == 1:
+        service = args[0]
+        return service, params
+
+    if service == 'weather':
+        if len(args) == 2:
+            if args[1] == 'now' or args[1] == 'today' or args[1] == 'tomorrow' or args[1] == 'tml':
+                params = args[1:]
+
+    return service, params
+
+def main(sms):
+    service, params = parser(sms)
+    if service == 'weather':
+        weather.handler(params)
+        
